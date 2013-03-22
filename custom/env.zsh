f54aee9c37690791214a3198441b5ac5410679d8
jphome <jphome98@gmail.com>
2013-03-22 09:47:56 +0800

    environment
    alias && export

---
+++ custom/env.zsh
@@ -0,0 +1,31 @@
+# some more ls aliases
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias lm='ls | more'
+
+####################################################################################################################
+
+alias cls='clear && ls'
+alias cla='clear && la'
+alias cll='clear && ll'
+alias rm='rm -rf'
+alias fuck_off='sudo shutdown -h now'
+alias hosts_update=''
+
+alias wine='env LANG=en_US wine'
+alias sourceinsight='wine /home/jphome/.wine/drive_c/Program\ Files/Source\ Insight\ 3/Insight3.Exe &'
+alias emacs='emacs -nw'
+
+export knowledge=/media/main/knowledge
+export xm=/media/main/project/xm
+
+####################################################################################################################
+
+
+export r11=/media/main/project/xm/code/R11
+export r10=/media/main/project/xm/code/R10
+export build_dir=/media/main/project/xm/code/Resource/ProductBuilder/General/General/MBD6016E-E
+
+# export PATH=$(PATH):/usr/local/hisi/DM8107/CodeSourcery/Sourcery_G++_Lite/bin:/usr/local/hisi/hisi-linux-nptl/arm-hisiv100-linux/target/bin:/usr/local/hisi/hisi-linux/x86-arm/arm-hisiv200-linux/target/bin:/usr/local/hisi/hisi-linux/x86-arm/arm-hisiv100-linux/target/bin:/usr/local/hisi/gm-linux/arm-none-linux-gnueabi-4.4.0_ARMv5TE/usr/bin/:/usr/local/hisi/crosstool/arm-linux/gcc-3.4.4-glibc-2.3.5/arm-linux/bin/:/usr/local/hisi/montavista/pro/devkit/arm/v5t_le/bin/:/usr/local/hisi/gm-linux/arm-linux/gcc-3.4.4-glibc-2.3.5/arm-linux/bin:/usr/local/hisi/hisi-linux/x86-arm-hi3515/gcc-3.4.3-uClibc-0.9.28/usr/bin:/usr/local/hisi/hisi-linux/x86-arm-hi3520/gcc-3.4.3-uClibc-0.9.28/usr/bin:/usr/local/hisi/hisi-linux/x86-arm/gcc-3.4.3-uClibc-0.9.29/usr/bin:/usr/local/hisi/hisi-linux/x86-arm/gcc-3.4.3-csl-uClibc-0.9.30-soft/bin/:/usr/local/hisi/hisi-linux/x86-arm/gcc-3.4.3-uClibc-0.9.28/usr/bin:/usr/local/hisi/hisi-linux/x86-arm/gcc-3.4.3-uClibc-0.9.28/bin:/usr/local/hisi/
